299e2194cf4bf6fdcd169998603ea3eadf67d73c
sashindionicus <sashindionicus465@gmail.com>
2021-04-15 18:59:16 +0300

    Realization decompozition with 2reducers(cash, customers)

--- src/index.js
+++ src/index.js
@@ -3,25 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './App';
+import {store} from "./store"
 
-const defaultState = {
-  cash: 0
-}
-
-
-
-const reducer = (state = defaultState, action) => {
-  switch(action.type) {
-    case "ADD_CASH":
-      return {...state, cash: state.cash + action.payload}
-    case "GET_CASH":
-      return {...state, cash: state.cash - action.payload}
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer)
 
 
 ReactDOM.render(
